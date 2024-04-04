5b2cb58c4aff0c7f743d9d126fb2ea302cb7c5f7
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-04-03 22:04:55 -0300

    Update Main.java

--- atvd8/1/2/Main.java
+++ atvd8/1/2/Main.java
@@ -1,1 +1,9 @@
+package exercicio_2;
+public class Main {
+    public static void main(String [] args){
+        Carro luconnes = new Carro("Com bunda ","10/10/2000","Renault");
+        System.out.println(luconnes.getModelo());
+        luconnes.setVelocidade_atual(-299);
+    }
+}
 
