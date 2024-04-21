7423cae2ec2818ce61f0096fe3282aef490411e8
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-04-21 18:41:11 -0300

    Create Funcionarios.java

---
+++ atvd_9/exercicio1/Funcionarios.java
@@ -0,0 +1,29 @@
+
+public class Funcionario {
+    private String nome;
+    private double salario;
+    private int idade;
+    public  Funcionario(String nome,int salario,int idade){
+        this.nome=nome;
+        this.salario=salario;
+        this.idade=idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
