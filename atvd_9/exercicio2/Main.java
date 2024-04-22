4ef43db95e69cf66a07dcffa2b53f9e96d809684
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-04-21 21:08:29 -0300

    Update Main.java

--- atvd_9/exercicio2/Main.java
+++ atvd_9/exercicio2/Main.java
@@ -1,1 +1,15 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Alunos aluno1 = new Alunos("João Silva", "12345");
+        Alunos aluno2 = new Alunos("Maria Santos", "67890");
+        aluno1.addNota(8.5);
+        aluno1.addNota(7.2);
+        aluno1.addNota(9.0);
+        aluno2.addNota(6.5);
+        aluno2.addNota(7.0);
+        aluno2.addNota(8.0);
+        aluno2.addNota(9.0); 
+        System.out.println("Média de notas de " + aluno1.getNome() + " (" + aluno1.getMatricula() + "): " + aluno1.getMedia());
+        System.out.println("Média de notas de " + aluno2.getNome() + " (" + aluno2.getMatricula() + "): " + aluno2.getMedia());
+    }
+}
