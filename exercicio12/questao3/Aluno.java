39b291a37e7882a92186f252623e8f867647ce2b
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-05-20 20:58:07 -0300

    Update Aluno.java

--- exercicio12/questao3/Aluno.java
+++ exercicio12/questao3/Aluno.java
@@ -1,1 +1,45 @@
 
+
+public class Alunos {
+    private String matricula;
+    private String nome;
+    private int idade;
+    private String curso;
+
+    // Construtor corrigido
+    public Alunos(String matricula, String nome, int idade, String curso){
+        this.matricula = matricula;
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }   
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
+
+    
+
+
