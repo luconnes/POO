977078528648b730e7fc7522775f75c202e598a3
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-05-20 16:12:05 -0300

    Update TesteHashSetCpf.java

--- exercicio12/exercicio2/TesteHashSetCpf.java
+++ exercicio12/exercicio2/TesteHashSetCpf.java
@@ -1,1 +1,18 @@
 
+import java.util.HashSet;
+import java.util.Scanner;
+public class TesteHashSetCpf {
+    public static void main(String[] args) {
+        Scanner leitor= new Scanner(System.in);
+        HashSet<String> cpfs = new HashSet<>();
+        for(int i=0;i<10;i++){
+            System.out.println("Informe o cpf: ");
+            String Cpf = leitor.nextLine();
+            cpfs.add(Cpf);
+            
+        }
+        System.out.println(cpfs);
+        leitor.close();
+    }
+}
+
