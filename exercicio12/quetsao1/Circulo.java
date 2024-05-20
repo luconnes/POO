d1d34cddf939832050f86ecdc52e283ef572affb
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-05-20 15:50:47 -0300

    Update Circulo.java

--- exercicio12/quetsao1/Circulo.java
+++ exercicio12/quetsao1/Circulo.java
@@ -1,1 +1,37 @@
 
+public class Circulo {
+    private double raio;
+    private final double pi = 3.14;
+    private double area;
+
+    // Construtor
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    // Método para calcular a área do círculo
+    public void calcularArea() {
+        this.area = (this.raio * this.raio) * pi;
+    }
+
+    // Getters e setters
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public double getPi() {
+        return pi;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+}
