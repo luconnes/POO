cf0a4171e3b96954286a7cdd52c11ab6df22761d
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-05-20 20:58:24 -0300

    Update Main.java

--- exercicio12/questao3/Main.java
+++ exercicio12/questao3/Main.java
@@ -1,1 +1,25 @@
 
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando uma lista de alunos
+        ArrayList<Alunos> listaDeAlunos = new ArrayList<>();
+
+        // Instanciando a classe CadastroAluno com a lista de alunos
+        CadastroAluno cadastro = new CadastroAluno(listaDeAlunos);
+
+        // Adicionando alguns alunos
+        Alunos aluno1 = new Alunos("202101", "João Silva", 20, "Engenharia");
+        Alunos aluno2 = new Alunos("202102", "Maria Souza", 22, "Medicina");
+        Alunos aluno3 = new Alunos("202103", "Carlos Pereira", 21, "Direito");
+
+        cadastro.adicionarNovoAluno(aluno1);
+        cadastro.adicionarNovoAluno(aluno2);
+        cadastro.adicionarNovoAluno(aluno3);
+
+       cadastro.retornarAlunosLista();
+    }
+}
